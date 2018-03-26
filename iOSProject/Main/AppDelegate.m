620c6630656cd8cc44fb3de940ddc1e227d7688e
dxw <739116989@qq.com>
2018-03-26 17:29:59 +0800

    1

--- iOSProject/Main/AppDelegate.m
+++ iOSProject/Main/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    //创建window
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
