e3abc687d8fa90e9413612f0d37154117061f28a
dxw <739116989@qq.com>
2018-03-26 15:53:31 +0800

    Initial Commit

---
+++ iOSProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSProject
+//
+//  Created by 360doc on 2018/3/26.
+//  Copyright © 2018年 dxw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
